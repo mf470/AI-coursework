844a914275b357c6f0afe77ce5019598a44ceb7b
mf470 <mf470@exeter.ac.uk>
2020-04-13 19:01:08 +0100

    Update K-means question.py

--- K-means question.py
+++ K-means question.py
@@ -1,105 +1,62 @@
 from sklearn.datasets import load_digits
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-import random as rnd
 import numpy as np
+from numpy.linalg import norm
 
 
-def matrixNorm(A, B):
-	"""
-	A norm or 'distance' function for matrices
-	:param A: n by n matrix
-	:param B: n by n matrix
-	:return: The norm of the difference of the matrices
-	"""
-	norm = 0
-	diff = [[A[i][j]-B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
-	for i in range(len(A)):
-		for j in range(len(A[0])):
-			norm += diff[i][j]*diff[i][j]
-	return norm
-
-
-def mean(matrices):
-	"""
-
-	:param matrices:
-	:return:
-	"""
-	mean = [[0]*len(matrices[0][0])]*len(matrices[0])
-	for i in range(len(matrices[0])):
-		for j in range(len(matrices[0][0])):
-			mean[i][j] = sum(matrix[i][j] for matrix in matrices)/len(matrices)
-	return mean
-
-
-def setCentroids(clusters):
-	"""
-
-	:param clusters:
-	:return:
-	"""
-	centroids = []
-	for cluster in clusters:
-		centroids.append(np.array(mean(cluster)))
-	return centroids
+def computeNorm(dataset, centroids):
+	distance = np.zeros((dataset.shape[0], len(centroids)))
+	for i in range(len(centroids)):
+		rowNorm = norm(dataset-centroids[i, :], axis=1)
+		distance[:, i] = np.square(rowNorm)
+	return distance
 
 
 def setClusters(dataset, centroids):
-	"""
-
-	:param dataset:
-	:param centroids:
-	:return:
-	"""
-	clusters = [[] for i in range(len(centroids))]
-	for matrix in dataset:
-		min = matrixNorm(centroids[0], matrix)
-		index = 0
-		for i in range(1, len(centroids)):
-			diff = matrixNorm(centroids[i], matrix)
-			if diff < min:
-				min, index = diff, i
-		(clusters[index]).append(matrix)
-
-	return clusters
+	norm = computeNorm(dataset, centroids)
+	return np.argmin(norm, axis=1)
 
 
-def k_means(dataset, k):
-	"""
+def setCetroids(dataset, labels, k):
+	centroids = np.zeros((k, dataset.shape[1]))
+	for i in range(k):
+		centroids[i, :] = np.mean(dataset[labels == i, :], axis=0)
+	return centroids
 
-	:param dataset:
-	:param k:
-	:return:
-	"""
-	i = 0
-	centroids = rnd.sample(dataset, k)
-	prevCentroids = []
-	while not np.array_equal(centroids, prevCentroids):
-		i += 1
+
+def calcError(dataset, labels, centroids):
+	distance = np.zeros(dataset.shape[0])
+	for i in range(dataset.shape[0]):
+		distance[i] = norm(dataset[i] - centroids[labels[i]])
+	return np.sum(np.square(distance))
+
+
+def k_means(dataset, k, maxit=300):
+	centroids = dataset[np.random.permutation(dataset.shape[0])[:k]]
+	for i in range(maxit):
 		prevCentroids = centroids
-		clusters = setClusters(dataset, centroids)
-		centroids = setCentroids(clusters)
-	return clusters
+		labels = setClusters(dataset, prevCentroids)
+		centroids = setCetroids(dataset, labels, k)
+		if np.all(centroids == prevCentroids):
+			return labels, centroids, error
+		error = calcError(dataset, labels, centroids)
 
 
 if __name__ == "__main__":
 	digits = load_digits()
 	trainingSet = [digits.data[i] for i in range(len(digits.images)) if i % 6 != 0]
 	testSet = [digits.data[i] for i in range(len(digits.images)) if i % 6 == 0]
-	#centroids, clusters = k_means(trainingSet, 10)
-	#testAssignment = setClusters(testSet, centroids)
+	# labels, centroids, error = k_means(digits.data, 10)
 	# plt.gray()
 	# for i in range(10):
-	# 	plt.matshow(testAssignment[i][0])
-	# 	plt.show()
-	# 	plt.matshow(clusters[i][0])
+	# 	plt.matshow(centroids[i].reshape((8, 8)))
 	# 	plt.show()
 
+
 	wcss = []
-	for i in range(1, 20):
-		kmeans = KMeans(n_clusters=i, max_iter=300, n_init=10, random_state=0)
-		kmeans.fit(trainingSet)
-		wcss.append(kmeans.inertia_)
-	plt.plot(range(1, 20), wcss)
+	for i in range(1, 16):
+		labels, centroids, error = k_means(digits.data, i)
+		wcss.append(error)
+	plt.plot(range(1, 16), wcss)
 	plt.show()
