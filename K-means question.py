bab8570876d6e0b224fcb2670ef6895d66760176
mf470 <mf470@exeter.ac.uk>
2020-04-14 16:33:11 +0100

    Docstrings

--- K-means question.py
+++ K-means question.py
@@ -6,6 +6,12 @@ from numpy.linalg import norm
 
 
 def computeNorm(dataset, centroids):
+	"""
+	Computes the distance from each element in the dataset to each centroid
+	:param dataset:
+	:param centroids:
+	:return:
+	"""
 	distance = np.zeros((dataset.shape[0], len(centroids)))
 	for i in range(len(centroids)):
 		rowNorm = norm(dataset-centroids[i, :], axis=1)
@@ -14,11 +20,24 @@ def computeNorm(dataset, centroids):
 
 
 def setClusters(dataset, centroids):
+	"""
+
+	:param dataset:
+	:param centroids:
+	:return:
+	"""
 	norm = computeNorm(dataset, centroids)
 	return np.argmin(norm, axis=1)
 
 
 def setCetroids(dataset, labels, k):
+	"""
+
+	:param dataset:
+	:param labels:
+	:param k:
+	:return:
+	"""
 	centroids = np.zeros((k, dataset.shape[1]))
 	for i in range(k):
 		centroids[i, :] = np.mean(dataset[labels == i, :], axis=0)
@@ -26,6 +45,13 @@ def setCetroids(dataset, labels, k):
 
 
 def calcError(dataset, labels, centroids):
+	"""
+
+	:param dataset:
+	:param labels:
+	:param centroids:
+	:return:
+	"""
 	distance = np.zeros(dataset.shape[0])
 	for i in range(dataset.shape[0]):
 		distance[i] = norm(dataset[i] - centroids[labels[i]])
@@ -33,29 +59,37 @@ def calcError(dataset, labels, centroids):
 
 
 def k_means(dataset, k, maxit=300):
+	"""
+
+	:param dataset:
+	:param k:
+	:param maxit:
+	:return:
+	"""
 	centroids = dataset[np.random.permutation(dataset.shape[0])[:k]]
 	for i in range(maxit):
 		prevCentroids = centroids
 		labels = setClusters(dataset, prevCentroids)
 		centroids = setCetroids(dataset, labels, k)
-		if np.all(centroids == prevCentroids):
-			return labels, centroids, error
 		error = calcError(dataset, labels, centroids)
+		if np.all(centroids == prevCentroids):
+			print(i)
+			return labels, centroids, error
 
 
 if __name__ == "__main__":
 	digits = load_digits()
 	trainingSet = [digits.data[i] for i in range(len(digits.images)) if i % 6 != 0]
 	testSet = [digits.data[i] for i in range(len(digits.images)) if i % 6 == 0]
-	# labels, centroids, error = k_means(digits.data, 10)
-	# plt.gray()
-	# for i in range(10):
-	# 	plt.matshow(centroids[i].reshape((8, 8)))
-	# 	plt.show()
+	plt.gray()
+	labels, centroids, error = k_means(digits.data, 10)
+	for i in range(10):
+		plt.matshow(centroids[i].reshape((8, 8)))
+		plt.show()
 
-	wcss = []
-	for i in range(1, 16):
-		labels, centroids, error = k_means(digits.data, i)
-		wcss.append(error)
-	plt.plot(range(1, 16), wcss)
-	plt.show()
+	# wcss = []
+	# for i in range(1, 16):
+	# 	labels, centroids, error = k_means(digits.data, i)
+	# 	wcss.append(error)
+	# plt.plot(range(1, 16), wcss)
+	# plt.show()
